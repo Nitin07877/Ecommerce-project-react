0696e28cd5905bca8730292fb6a6da43f7d34dde
Nitin <nitinchaudhary12334@gmail.com>
2026-01-01 11:29:10 +0530

    correct notFoundPage

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,12 +1,13 @@
 import {Header} from '../components/Header';
+import './NotFoundPage.css'
 export function NotFoundPage (){
     return(
         <>
         <title>404 Page Not Found</title>
-        <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+        <link rel="icon" type="image/svg+xml" href="error-favicon.png" />
         <Header/>
         <div className="not-found-message">
-            Page not found
+            <p>Page not found!</p>
         </div>
         </>
     );
