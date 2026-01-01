66645e62292f33c83f5f7d44c83b04c59ae0f597
Nitin <nitinchaudhary12334@gmail.com>
2026-01-01 11:21:35 +0530

    error page and correct some links and images

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,8 +3,10 @@ import { HomePage } from './pages/Homepage'
 import { CheckoutPage } from './pages/Checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 import './App.css'
+
 
 function App() {
   
@@ -16,6 +18,7 @@ function App() {
       <Route path='checkout' element={<CheckoutPage/>}/>
       <Route path='orders' element={<OrdersPage/>}/>
       <Route path='tracking' element={<TrackingPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
 
       
     </Routes>
