65a4d6ea2d6a790b477687834e30586e8c143174
Nitin <nitinchaudhary12334@gmail.com>
2026-01-04 13:08:09 +0530

    create , update delivery option

--- ecommerce-project/src/pages/Checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/Checkout/CheckoutPage.jsx
@@ -9,7 +9,7 @@ import { PaymentSummary } from './PaymentSummary';
 
 
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart , loadCart}) {
     const [deliveryOptions, setDeliveryOptions] = useState([]);
     const [paymentSummary,setPaymentSummary ] = useState(null);
 
@@ -27,7 +27,7 @@ export function CheckoutPage({ cart }) {
         };
         fetchCheckoutData();
             
-    }, [])
+    }, [cart])
     return (
         <>
             <title>Checkout</title>
@@ -38,7 +38,7 @@ export function CheckoutPage({ cart }) {
                 <div className="page-title">Review your order</div>
 
                 <div className="checkout-grid">
-                    <OrderSummary cart={cart} deliveryOptions={deliveryOptions}/>
+                    <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart= {loadCart}/>
                     <PaymentSummary paymentSummary={paymentSummary}/>
                     
                 </div>
--- ecommerce-project/src/pages/Checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/Checkout/OrderSummary.jsx
@@ -1,7 +1,7 @@
 import { CartItemDetails } from './CartItemDetails';
 import { DeliveryOptions } from './DeliveryOptions';
 import { DeliveryDate } from './DeliveryDate';
-export function OrderSummary({cart,deliveryOptions}){
+export function OrderSummary({cart,deliveryOptions ,loadCart}){
     return (
         <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
@@ -10,7 +10,7 @@ export function OrderSummary({cart,deliveryOptions}){
                         <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
                         <div className = "cart-item-details-grid">
                             <CartItemDetails cartItem={cartItem}/>
-                            <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem}/>
+                            <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart = {loadCart} />
                         </div>
                     </div>
                 );
