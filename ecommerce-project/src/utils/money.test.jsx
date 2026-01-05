7d07fcaf99ac1830150df62ce4c0a115508ef708
Nitin <nitinchaudhary12334@gmail.com>
2026-01-05 11:42:46 +0530

    create integration test for production component

--- ecommerce-project/src/utils/money.test.jsx
+++ ecommerce-project/src/utils/money.test.jsx
@@ -6,6 +6,6 @@ describe ('format Money' , ()=>{
         expect(formatMoney(1999)).toBe('$19.99');
     });
     it('displays 2 decimals ', ()=>{
-        expect(formatMoney(1090)).toBe('$10.90');
+        expect(formatMoney(100)).toBe('$1.00');
     });
 })
